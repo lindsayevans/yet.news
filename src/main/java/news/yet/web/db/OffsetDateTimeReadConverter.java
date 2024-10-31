b3f24613b8f359abdb927d45614c692eb24551ad
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-11-01 10:03:50 +1100

    refactor: add NonNull annotations

--- src/main/java/news/yet/web/db/OffsetDateTimeReadConverter.java
+++ src/main/java/news/yet/web/db/OffsetDateTimeReadConverter.java
@@ -5,10 +5,11 @@ import java.time.ZoneOffset;
 import java.util.Date;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 public class OffsetDateTimeReadConverter implements Converter<Date, OffsetDateTime> {
     @Override
-    public OffsetDateTime convert(Date date) {
+    public OffsetDateTime convert(@NonNull Date date) {
         return date.toInstant().atOffset(ZoneOffset.UTC);
     }
 }
--- src/main/java/news/yet/web/db/OffsetDateTimeWriteConverter.java
+++ src/main/java/news/yet/web/db/OffsetDateTimeWriteConverter.java
@@ -4,10 +4,11 @@ import java.time.OffsetDateTime;
 import java.util.Date;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
 
 public class OffsetDateTimeWriteConverter implements Converter<OffsetDateTime, Date> {
     @Override
-    public Date convert(OffsetDateTime offsetDateTime) {
+    public Date convert(@NonNull OffsetDateTime offsetDateTime) {
         return Date.from(offsetDateTime.toInstant());
     }
 }
