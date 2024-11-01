837aef48aba578a3fd9fd37af2eb74b3b5f21d52
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-11-01 14:06:47 +1100

    refactor: remove unused import

--- src/main/java/news/yet/web/questions/QuestionController.java
+++ src/main/java/news/yet/web/questions/QuestionController.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
