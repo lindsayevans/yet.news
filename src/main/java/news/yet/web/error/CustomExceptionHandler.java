829998c08d00fa7539eb401e5c041f4e02b4b02d
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-11-07 11:54:21 +1100

    fix: send correct status code + retry-after header on rate limit

--- src/main/java/news/yet/web/error/CustomExceptionHandler.java
+++ src/main/java/news/yet/web/error/CustomExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
@@ -14,9 +15,14 @@ public class CustomExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
 
     @ExceptionHandler(RequestNotPermitted.class)
-    public String handleRequestNotPermitted(RequestNotPermitted ex, HttpServletRequest request) {
+    public String handleRequestNotPermitted(RequestNotPermitted ex, HttpServletRequest request,
+            HttpServletResponse response) {
         logger.warn("Request to path '{}' is blocked due to rate-limiting. {}",
                 request.getRequestURI(), ex.getMessage());
+
+        response.addHeader("Retry-After", "60s");
+        response.setStatus(429);
+
         return "error-429";
     }
 }
